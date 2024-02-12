c448fdd560ad697709ba00321ed938bf1ceb3ec9
Ugur Kartal <kartalugur@gmail.com>
2024-02-12 11:19:07 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+
+## Coding: Date with java.time
+
+Create a new project in IntelliJ (please use a Java application) and use the java.time classes to perform various date operations.
+
+-   Step 1: Write code to output the current date and time.
+-   Step 2: Add a timespan of 2 weeks to the current date and output the new date.
+-   Step 3: Compare the current date with a specified future date and output whether the current date is before or after the specified date.
+-   Step 4: Calculate the difference in days between two arbitrary dates and output the result.
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime afterTwoWeeks = now.plusWeeks(2);
+        DateTimeFormatter dateFormatter=DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
+        //Step 1: Write code to output the current date and time.
+        System.out.println("Now (in date and time): " + now.format(dateTimeFormatter));
+
+        //Step 2: Add a timespan of 2 weeks to the current date and output the new date.
+        System.out.println("After 2 weeks (in date): " + afterTwoWeeks.format(dateFormatter));
+
+        //Step 3: Compare the current date with a specified future date and output whether the current date is before or after the specified date.
+        System.out.println(now.format(dateFormatter) + (now.isBefore(afterTwoWeeks) ? " is before " : " is after ") + afterTwoWeeks.format(dateFormatter));
+
+        //Step 4: Calculate the difference in days between two arbitrary dates and output the result.
+        System.out.printf("There are %d days between %s and %s%n ", (int) ChronoUnit.DAYS.between(now, afterTwoWeeks), now.format(dateFormatter).toString(), afterTwoWeeks.format(dateFormatter).toString());
+    }
+}
